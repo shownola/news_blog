a256ea5c49813a431ceb905679bc0648ec8ed9d4
shownola <swasieko@gmail.com>
2021-03-26 07:39:49 -0700

    Add devise for users and articles controller and seeds for article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article has been created'
+      redirect_to @article
+    else
+      flash.now[:alert] = 'There was an error try again'
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = 'Article has been updated'
+      redirect_to @articles
+    else
+      flash[:alert] = 'There was an error, try again'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content, :category, :user_id)
+  end
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 5.times do |user|
+#   User.create!(email: 'sherry@email.com', password: 'sherry')
+# end
+
+10.times do |article|
+  Article.create!(title: "Article Title #{article}",
+                  content: "Godfather ipsum dolor sit amet. When they come... they come at what you love. Te salut, Don Corleone. Leave the gun. Take the cannoli. We're both part of the same hypocrisy, senator, but never think it applies to my family.
+
+                  It's a Sicilian message. It means Luca Brasi sleeps with the fishes. Is that why you slapped my brother around in public? Don't ever give an order like that again. Not while I'm alive. My father taught me many things here - he taught me in this room. He taught me: keep your friends close, but your enemies closer. Only don't tell me you're innocent. Because it insults my intelligence and makes me very angry."
+                  category: 'Coding',
+                   )
+                 end
