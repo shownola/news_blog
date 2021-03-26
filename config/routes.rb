f4afd05774d83a9491e176a7c5a3b3cb76d75f7b
shownola <swasieko@gmail.com>
2021-03-26 12:25:54 -0700

    Add users index and route add nav, show and articles index pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
-  devise_for :users
+
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'home#index'
   resources :articles
+
+  get 'users',  to: 'users#index'
 end
